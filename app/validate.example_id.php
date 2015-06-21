afd5d949eae713b02e01f8102053af1403cf2e65
foospidy <foospidy@users.noreply.github.com>
2015-06-21 16:51:35 -0400

    Update validate.example_id.php

--- app/validate.example_id.php
+++ app/validate.example_id.php
@@ -1,7 +1,8 @@
 <?php
 /// set the parameter name.
 $parameter = 'example_id';
-/// set expected method for this parameter. options are 'GET', 'POST', 'PUT', 'DELETE';
+/// set expected method for this parameter. options are 'GET', 'POST', 'PUT', 'DELETE'.
+/// for cookies use 'COOKIE'.
 $method    = 'GET';
 /// set the regular expression to validate the parameter.
 $regex     = '/^[a-z0-9]{1,25}$/';
