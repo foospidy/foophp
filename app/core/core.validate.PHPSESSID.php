729cb3089cc9e7f7f99cf776fbe0df004f437385
foospidy <foospidy@users.noreply.github.com>
2015-06-21 16:50:25 -0400

    Create core.validate.PHPSESSID.php

---
+++ app/core/core.validate.PHPSESSID.php
@@ -0,0 +1,12 @@
+<?php
+/// set the parameter name.
+$parameter = 'PHPSESSID';
+/// set expected method for this parameter. options are 'GET', 'POST', or 'GET|POST'.
+/// for cookies use 'COOKIE'
+$method    = 'COOKIE';
+/// set the regular expression to validate the parameter.
+$regex     = '/^[a-z0-9-]{1,33}$/';
+
+////////////////////////////////////////////
+validate_input($parameter, $method, $regex);
+?>
