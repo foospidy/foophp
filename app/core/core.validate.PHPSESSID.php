56ea3a60fa7eb7dcde1321176f25cd049b30d444
foospidy <foospidy@users.noreply.github.com>
2015-06-21 16:51:59 -0400

    Update core.validate.PHPSESSID.php

--- app/core/core.validate.PHPSESSID.php
+++ app/core/core.validate.PHPSESSID.php
@@ -5,7 +5,7 @@ $parameter = 'PHPSESSID';
 /// for cookies use 'COOKIE'
 $method    = 'COOKIE';
 /// set the regular expression to validate the parameter.
-$regex     = '/^[a-z0-9-]{1,33}$/';
+$regex     = '/^[a-z0-9]{1,33}$/';
 
 ////////////////////////////////////////////
 validate_input($parameter, $method, $regex);
